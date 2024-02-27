5aa93ecffdc469438fd56d7894531e93602f2285
yaroslavyakovkin <yaroslav.y2010@ya.ru>
2024-02-27 16:57:46 +0300

    attemp

--- tray.py
+++ tray.py
@@ -1,56 +1,57 @@
 import pystray as tr
-import os
-import subprocess as sub
-from winotify import Notification as N
+from aiogram import Dispatcher
+# import os
+# import subprocess as sub
+# from winotify import Notification as N
 from PIL import Image
 from win32 import win32print as p
-from database.sql import db_start, edit_defaults, get_defaults
+from database.sql import edit_defaults, get_defaults
 
     
-db_start()
-img = Image.open('assets\\alpha.png')
+# db_start()
+async def tray(dp, bot):
+    img = Image.open('assets\\alpha.png')
 
-def get_printers():
-    printers = ['Default']
-    for printer in p.EnumPrinters(p.PRINTER_ENUM_CONNECTIONS | p.PRINTER_ENUM_LOCAL):
-        printers.append(printer[2])
-    return printers
+    def get_printers():
+        printers = ['Default']
+        for printer in p.EnumPrinters(p.PRINTER_ENUM_CONNECTIONS | p.PRINTER_ENUM_LOCAL):
+            printers.append(printer[2])
+        return printers
 
-def get_menu():
-    global menu
-    global selected
-    menu = []
-    m_items = []
-    selected = get_defaults('printer')
+    def get_menu():
+        global menu
+        global selected
+        menu = []
+        m_items = []
+        selected = get_defaults('printer')
 
-    for printer in get_printers():
-        m_items += [tr.MenuItem(printer, select, lambda _, item = printer: item == selected)]
+        for printer in get_printers():
+            m_items += [tr.MenuItem(printer, select, lambda _, item = printer: item == selected)]
 
-    m_items += [tr.MenuItem('Обновить', select)]
-    menu += [tr.MenuItem('Принтеры', tr.Menu(*m_items))]
-    menu += [tr.MenuItem('Выход', select)]
-    return menu
+        m_items += [tr.MenuItem('Обновить', select)]
+        menu += [tr.MenuItem('Принтеры', tr.Menu(*m_items))]
+        menu += [tr.MenuItem('Выход', select)]
+        return menu
 
-def select(icon, item):
-    text = item.text
-    if text=='Выход':
-        icon.stop()
-    elif text=='Обновить':None
-    else:edit_defaults('printer', text)
-    get_menu()
-    icon.update_menu()
+    def select(icon, item):
+        text = item.text
+        if text=='Выход':
+            icon.stop()
+        elif text=='Обновить':None
+        else:edit_defaults('printer', text)
+        get_menu()
+        icon.update_menu()
 
-process = sub.Popen('.venv/Scripts/python bot.py', creationflags=sub.CREATE_NO_WINDOW)
+    menu = get_menu()
+    icon = tr.Icon('Office Printer', img, 'Office Printer', tr.Menu(*menu))
+    icon.run()
+    dp.stop_polling(bot)
+# process = sub.Popen('.venv/Scripts/python bot.py', creationflags=sub.CREATE_NO_WINDOW)
+# process.terminate()
 
-menu = get_menu()
-icon = tr.Icon('Office Printer', img, 'Office Printer', tr.Menu(*menu))
-icon.run()
-
-process.terminate()
-
-ico = os.path.join(os.getcwd(),'assets\logo.jpg')
-toast = N(app_id='Office Print',
-            title='Бот остановлен!',
-            msg='Бот успешно остановлен.\nДо новых встреч!',
-            icon=ico)
-toast.show()
+# ico = os.path.join(os.getcwd(),'assets\logo.jpg')
+# toast = N(app_id='Office Print',
+#             title='Бот остановлен!',
+#             msg='Бот успешно остановлен.\nДо новых встреч!',
+#             icon=ico)
+# toast.show()
